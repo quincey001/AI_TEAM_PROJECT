53c4d4893a3007bf3d2412dda09d86316e570ff1
Mahislami <61377584+Mahislami@users.noreply.github.com>
2022-04-11 01:46:13 +0100

    Update cspAgent.py

--- SudokuCSPAgent/cspAgent.py
+++ SudokuCSPAgent/cspAgent.py
@@ -1,4 +1,6 @@
 from pulp import *
+import time
+from profile import profile
 
 class CSP():
 
@@ -6,78 +8,92 @@ class CSP():
         self.Sequence = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
         # The Vals, Rows and Cols sequences all follow this form
-        self.Vals = self.Sequence
-        self.Rows = self.Sequence
-        self.Cols = self.Sequence
+        self.values = self.Sequence
+        self.row = self.Sequence
+        self.column = self.Sequence
 
         # The boxes list is created, with the row and column index of each square in each box
         self.Boxes =[]
         # The prob variable is created to contain the problem data        
-        self.prob = LpProblem("Sudoku Problem",LpMinimize)
+        self.constraint = LpProblem("Sudoku Problem",LpMinimize)
 
         # The problem variables are created
-        self.choices = LpVariable.dicts("Choice",(self.Vals,self.Rows,self.Cols),0,1,LpInteger)
+        self.choices = LpVariable.dicts("Choice",(self.values,self.row,self.column),0,1,LpInteger)
         # A list of strings from "1" to "9" is created
-
         self.table = table
 
 
     def create_box_list(self):
         for i in range(3):
             for j in range(3):
-                self.Boxes += [[(self.Rows[3*i+k], self.Cols[3*j+l]) for k in range(3) for l in range(3)]]
+                self.Boxes += [[(self.row[3*i+k], self.column[3*j+l]) for k in range(3) for l in range(3)]]
 
    # A constraint ensuring that only one value can be in each square is created 
     def one_value_in_cell(self):
-        for r in self.Rows:
-            for c in self.Cols:
-                self.prob += lpSum([self.choices[v][r][c] for v in self.Vals]) == 1, ""
+        for r in self.row:
+            for c in self.column:
+                new_constraint = []
+                for v in self.values:
+                    new_constraint.append(self.choices[v][r][c])
+                self.constraint += lpSum(new_constraint) == 1, ""
     
     def row_constraint(self):
-        self.prob += 0, "Arbitrary Objective Function"
+        self.constraint += 0, "Arbitrary Objective Function"
         # The row, column and box constraints are added for each value
-        for v in self.Vals:
-            for r in self.Rows:
-                self.prob += lpSum([self.choices[v][r][c] for c in self.Cols]) == 1,""
+        for v in self.values:
+            for r in self.row:
+                new_constraint = []
+                for c in self.column:
+                    new_constraint.append(self.choices[v][r][c])
+                self.constraint += lpSum(new_constraint) == 1,""
         
-            for c in self.Cols:
-                self.prob += lpSum([self.choices[v][r][c] for r in self.Rows]) == 1,""
+            for c in self.column:
+                new_constraint = []
+                for r in self.row:
+                    new_constraint.append(self.choices[v][r][c])
+                self.constraint += lpSum(new_constraint) == 1,""
 
             for b in self.Boxes:
-                self.prob += lpSum([self.choices[v][r][c] for (r,c) in b]) == 1,""
+                new_constraint = []
+                for (r,c) in b:
+                    new_constraint.append(self.choices[v][r][c])
+                self.constraint += lpSum(new_constraint) == 1,""
   
     def extisting_constraint(self):
         for row in range(9):
             for col in range(9):
                 if self.table[row][col] != 0:
-                    self.prob += self.choices[str(self.table[row][col])][str(row+1)][str(col+1)] == 1,""
+                    self.constraint += self.choices[str(self.table[row][col])][str(row+1)][str(col+1)] == 1,""
     
     def writeLP(self):
         # The problem data is written to an .lp file
-        self.prob.writeLP("Sudoku.lp")
+        self.constraint.writeLP("Sudoku.lp")
 
+    @profile
     def solve(self):
         # The problem is solved using PuLP's choice of Solver
+        start = time.time()
         self.create_box_list()
         self.one_value_in_cell()
         self.row_constraint()
         self.extisting_constraint()
         self.writeLP()
-        self.prob.solve()
+        self.constraint.solve()
+        end = time.time()
+        print(end - start)
         self.printStatus()
         self.print_and_write()
-        #print(self.__str__())
 
     def printStatus(self):
         #The status of the solution is printed to the screen
-        print("Status:", LpStatus[self.prob.status])
+        print("Status:", LpStatus[self.constraint.status])
 
     def print_and_write(self):
         sudokuout = ""
         # The solution is written to the sudokuout.txt file 
-        for r in self.Rows:
-            for c in self.Cols:
-                for v in self.Vals:
+        for r in self.row:
+            for c in self.column:
+                for v in self.values:
                     if value(self.choices[v][r][c])==1:             
                         sudokuout = sudokuout + (str(v) + " ")   
                         if c == "9":
